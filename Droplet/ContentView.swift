4d10e922e0c6c21cce33070cb13fc34ac36f00f3
Evdokimov Denis <analitik9@gmail.com>
2023-12-25 13:37:46 +0300

    Initial Commit

---
+++ Droplet/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  Droplet
+//
+//  Created by Denis Evdokimov on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var location: CGPoint = CGPoint(x: UIScreen.main.bounds.midX,
+                                                   y: UIScreen.main.bounds.midY)
+    @State private var offSet: CGSize = .zero
+    
+    var body: some View {
+        Rectangle()
+            .fill(
+                RadialGradient(
+                    gradient: .init(colors: [Color.yellow, Color.red]),
+                    center: .center,
+                    startRadius: 60,
+                    endRadius: 120
+                )
+            )
+            .mask {
+                Canvas { context, size in
+                    context.addFilter(.alphaThreshold(min: 0.5))
+                    context.addFilter(.blur(radius: 25))
+                    let shapeOne = context.resolveSymbol(id: 1)!
+                    let shapeTwo = context.resolveSymbol(id: 2)!
+        
+                    context.drawLayer { context in
+                        context.draw(shapeOne, at: location)
+                        context.draw(shapeTwo, at: location)
+                    }
+                } symbols: {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .tag(1)
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .offset(x: offSet.width, y: offSet.height)
+                        .tag(2)
+                    
+                }
+            }
+            .overlay(content: {
+                Image(systemName: "cloud.sun.rain.fill")
+                    .symbolRenderingMode(.hierarchical)
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundStyle( .white )
+                                .offset(x: offSet.width, y: offSet.height)
+            })
+            .ignoresSafeArea()
+            .gesture(DragGesture()
+                .onChanged { value in
+                    offSet = value.translation
+                }.onEnded { value in
+                    withAnimation(.spring(bounce: 0.3)) {
+                        offSet = .zero
+                    }
+                })
+        }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Droplet/DropletApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropletApp.swift
+//  Droplet
+//
+//  Created by Denis Evdokimov on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DropletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
